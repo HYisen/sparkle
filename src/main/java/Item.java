dc4980129c01d63d63c1aeea7f969bc8787d8307
Alex <alexhyisen@gmail.com>
2018-11-07 17:57:11 +0800

    make hello world works

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -2,7 +2,7 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class Item implements Serializable {
+public class Item implements Serializable, Comparable<Item> {
     private LocalDateTime time;
     private String uid;
     private String key;
@@ -10,8 +10,20 @@ public class Item implements Serializable {
     private int frequent;
     private String url;
 
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public int compareTo(Item o) {
+        return time.compareTo(o.time);
+    }
+
+    public Item() {
+    }
 
     public Item(String line) {
+        load(line);
+    }
+
+    public void load(String line) {
         String[] words = line.split("\t");
         this.time = LocalDateTime.parse(words[0], DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
         this.uid = words[1];
@@ -31,11 +43,47 @@ public class Item implements Serializable {
         return time;
     }
 
+    public String getUid() {
+        return uid;
+    }
+
     public String getKey() {
         return key;
     }
 
+    public int getPosition() {
+        return position;
+    }
+
+    public int getFrequent() {
+        return frequent;
+    }
+
     public String getUrl() {
         return url;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void setFrequent(int frequent) {
+        this.frequent = frequent;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,6 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf()
@@ -13,12 +10,31 @@ public class Main {
 
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+//        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+//
+//        JavaRDD<Integer> rdd = sc.parallelize(data);
+//
+//        System.out.println(rdd.reduce((a, b) -> a + b));
+//
+//        System.out.println(rdd.count());
 
-        JavaRDD<Integer> rdd = sc.parallelize(data);
+        JavaRDD<String> input = sc.textFile("/home/alex/code/02/data");
 
-        System.out.println(rdd.reduce((a, b) -> a + b));
+        System.out.println(input.count());
 
-        System.out.println(rdd.count());
+        JavaRDD<Item> data = input.map(Item::new);
+
+        System.out.println(data.map(Item::getFrequent).reduce((a, b) -> a + b));
+
+        String reduce = data.map(Item::toString).reduce((a, b) -> a + "\n" + b);
+        System.out.println(reduce);
+//        data.foreach(System.out::println);
+
+
+//        System.out.println(data.count());
+//        long count = data.map(Item::toString).count();
+//        System.out.println(count);
+        System.out.println(data.top(1).get(0).toString());
+
     }
 }
