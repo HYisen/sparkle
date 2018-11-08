b2badde12e3cb3b2e97559944336c92dae415345
Alex <alexhyisen@gmail.com>
2018-11-08 15:58:40 +0800

    add a procedure that detect robots

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.storage.StorageLevel;
 import scala.Tuple2;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.IntSummaryStatistics;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -102,5 +106,49 @@ public class Main {
         tl.log("05");
 
         data.unpersist();
+        Utility.output("robots", String.join("\n",
+                data
+                        .groupBy(v -> new Tuple2<>(v.getUid(), v.getKey()))
+                        .filter(v -> StreamSupport.stream(v._2.spliterator(), false).count() > 10)
+                        .aggregateByKey(
+                                new HashMap<>(10),
+                                (Function2<HashMap<Integer, Integer>, Iterable<Record>, HashMap<Integer, Integer>>)
+                                        (map, records) -> {
+                                            HashMap<Integer, Integer> rtn = new HashMap<>(map);
+                                            records.forEach(r -> update(rtn, r.getPosition(), 1));
+                                            return rtn;
+                                        },
+                                (mapL, mapR) -> {
+                                    HashMap<Integer, Integer> rtn = new HashMap<>(mapL);
+                                    mapR.forEach((k, v) -> update(rtn, k, v));
+                                    return mapL;
+                                })
+                        .mapValues(v -> {
+                            IntSummaryStatistics stat = v.values().stream()
+                                    .collect(Collectors.summarizingInt(Integer::intValue));
+                            int size = (int) stat.getSum();
+                            if (v.size() < v.keySet().stream().max(Integer::compareTo).orElse(0)) {
+                                return new Tuple2<>(size, size);
+                            }
+                            int depth = stat.getMax() > 1 ? stat.getMax() - 1 : stat.getMax();
+                            int miss = v.values().stream().mapToInt(n -> depth - n).filter(n -> n > 0).sum();
+                            return new Tuple2<>(miss, size);
+                        })
+                        .mapToPair(v -> new Tuple2<>(v._2, v._1))
+                        .sortByKey(Utility.SerializableComparator.serialize((lhs, rhs) ->
+                                Integer.compare(score(lhs), score(rhs))), false)
+                        .map(v -> String.format("%s\t%10d\t%10d\t%s\t", v._2._1, v._1._1, v._1._2, v._2._2))
+                        .collect()
+        ));
+        tl.log("06");
+
+    }
+
+    private static int score(Tuple2<Integer, Integer> orig) {
+        return -orig._1 * 20 + orig._2;
+    }
+
+    private static Integer update(HashMap<Integer, Integer> rtn, Integer k, Integer v) {
+        return rtn.put(k, rtn.getOrDefault(k, 0) + v);
     }
 }
--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
+import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Comparator;
 
 public class Utility {
     public static void output(String name, String... lines) throws IOException {
@@ -14,6 +16,13 @@ public class Utility {
             line += "\n";
             System.out.print(line);
             Files.write(path, line.getBytes(), StandardOpenOption.APPEND);
+        }
+    }
+
+    //copied solution from https://stackoverflow.com/questions/19433135/notserializableexception-when-sorting-in-spark
+    public interface SerializableComparator<T> extends Comparator<T>, Serializable {
+        static <T> SerializableComparator<T> serialize(SerializableComparator<T> comparator) {
+            return comparator;
         }
     }
 }
