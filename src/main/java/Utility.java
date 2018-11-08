bef4c97a13eb839f49fd37c69de654290264b92f
Alex <alexhyisen@gmail.com>
2018-11-08 22:40:34 +0800

    add a roll function & improve output function

--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -5,16 +5,26 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Comparator;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 public class Utility {
     public static void output(String name, String... lines) throws IOException {
+        output(name, true, lines);
+    }
+
+    public static void output(String name, boolean writeConsole, String... lines) throws IOException {
         Path path = Paths.get("output", name);
         if (!Files.exists(path)) {
             Files.createFile(path);
         }
         for (String line : lines) {
             line += "\n";
-            System.out.print(line);
+            if (writeConsole) {
+                System.out.print(line);
+            }
             Files.write(path, line.getBytes(), StandardOpenOption.APPEND);
         }
     }
@@ -24,5 +34,26 @@ public class Utility {
         static <T> SerializableComparator<T> serialize(SerializableComparator<T> comparator) {
             return comparator;
         }
+    }
+
+    public static void roll(int rounds) throws IOException {
+        TimeLogger tl = new TimeLogger("dice");
+        tl.start();
+        for (int i = 0; i < rounds; i++) {
+            Random random = new Random(17);
+            ConcurrentHashMap<Integer, Long> data = new ConcurrentHashMap<>(60);
+            LongStream.range(0, 43545352)
+                    .parallel()
+                    .mapToInt(v -> random.nextInt(60))
+                    .forEach(v -> data.put(v, data.getOrDefault(v, 0L) + 1));
+            tl.log("loop " + i + " completed");
+            Utility.output("result",
+                    data.values().stream().map(Object::toString).collect(Collectors.joining("\n")));
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        roll(1000);
     }
 }
